55ee0874322bc9d2f2b1b39a84494ae300bb70b4
Alina Shabalina <makosei@list.ru>
2023-04-28 21:39:03 +0200

    Initial commit

---
+++ tocha_design_vue/tocha_design_vue/src/pages/index/index-page.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="index-page__wrapper">
+      <header>
+      <h1>Hello</h1>
+    </header>
+  <SearchForm class="index-page__search"/>
+  <ResultField temp="1" speed="10 km/h" direction="110"/>
+  </div>
+    </template>
+    
+    <script lang="ts" src="./index-page.ts"></script>
+    <style lang="scss" src="./index-page.scss">
+    </style>
