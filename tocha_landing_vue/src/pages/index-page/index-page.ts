1a4f5904ae88c39e6ddf0133fabed9f00deae68c
Alina Shabalina <makosei@list.ru>
2023-05-01 21:21:41 +0200

    Add footer and header initial

---
+++ tocha_landing_vue/src/pages/index-page/index-page.ts
@@ -0,0 +1,14 @@
+import { defineComponent } from "vue";
+import PageFooter from "@/components/page-footer/page-footer.vue";
+import PageHeader from "@/components/page-header/page-header.vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  components: {
+    PageFooter: PageFooter,
+    PageHeader: PageHeader,
+  },
+  setup() {},
+
+  return: {},
+});
---
+++ tocha_landing_vue/src/pages/index-page/index-page.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="index-page__wrapper">
+    <header class="index-page__header">
+      <PageHeader />
+    </header>
+    <main></main>
+    <footer class="index-page__footer">
+    <PageFooter />
+  </footer>
+  </div>
+    </template>
+    
+    <script lang="ts" src="./index-page.ts"></script>
+    <style lang="scss" src="./index-page.scss">
+    </style>
