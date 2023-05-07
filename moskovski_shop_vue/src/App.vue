f4dd8ae6df865fadb21c029cef04fd893fde731d
Alina Shabalina <makosei@list.ru>
2023-05-07 21:05:26 +0200

    Add header

--- moskovski_shop_vue/src/App.vue
+++ moskovski_shop_vue/src/App.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-</script>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import IndexPage from '@/pages/index-page/index-page.vue';
+@Options({
+    components: {
+      IndexPage: IndexPage,
+    },
+  })
+  export default class App extends Vue {}
+  </script>
 
 <template>
-
   <main>
+<IndexPage />
   </main>
 </template>
 
